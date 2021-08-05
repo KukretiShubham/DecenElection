ce80e9421bbd514f0f37ac00f55815a60f21bfc5
Shubham Kukreti <shubhamkukreti204@gmail.com>
2021-08-06 02:39:23 +0530

    First Dapp application

---
+++ contracts/Election.sol
@@ -0,0 +1,11 @@
+pragma solidity >=0.4.22 <0.8.0;
+
+contract Election{ 
+	// Store Candidate
+	// Read Candidate
+	// Constructor
+	string public candidate;
+	constructor () public {
+		candidate = "Candidate 1";
+	}
+}
